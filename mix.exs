4e03ef2676fd0289f76bf79fe8761b9dfc75e726
Christopher Adams <christopher@fabricatorz.com>
2016-08-30 12:12:35 +0800

    Add package information

--- mix.exs
+++ mix.exs
@@ -1,12 +1,17 @@
 defmodule Multiplex.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :multiplex,
-     version: "0.1.0",
+     version: @version,
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     docs: [extras: ["README.md"]],
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
@@ -17,5 +22,20 @@ defmodule Multiplex.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.2.0"},
      {:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp description do
+    """
+    Phoenix router macro to dispatch multiple HTTP methods to a Plug
+    """
+  end
+
+  defp package do
+    [
+     name: :multiplex,
+     maintainers: ["Christopher Adams"],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/christopheradams/multiplex"}
+    ]
   end
 end
